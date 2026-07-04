0248777edb32bed5f651fcc2ca640eb2f17b6430
Yuliantii20 <awaliyahyulianti4@gmail.com>
2026-07-04 17:33:01 +0700

    Fix GitHub Actions APK release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
     namespace = "com.example.uas_mobile_lanjut"
 
     compileSdk = 35
-
     ndkVersion = flutter.ndkVersion
 
     defaultConfig {
         applicationId = "com.example.uas_mobile_lanjut"
 
-        minSdk = flutter.minSdkVersion
+        // Isar membutuhkan minimal SDK 21
+        minSdk = 21
+
         targetSdk = 35
 
         versionCode = flutter.versionCode
@@ -16,17 +23,23 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
+}
+
+flutter {
+    source = "../.."
 }
